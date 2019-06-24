e6e6e97abc511b95c45783b914dee5f499352eb9
Luke-6723 <lukemorris2015@hotmail.com>
2019-06-24 21:39:25 +0100

    commands actually work now

--- commands/mute.js
+++ commands/mute.js
@@ -1,15 +1,18 @@
 
 exports.run = (client, msg, args, server_settings) => {
-    if(!msg.guild.member(client.user.id).permissions.has("MANAGE_MEMBERS")) return msg.channel.send("I don't have permission to do this.");
-    if(!msg.guild.member(msg.author.id).permissions.has("MANAGE_MEMBERS")) return msg.channel.send("You don't have permission to do this.");
-    id_to_mute = args[0].replace("/[<@!>]/g", "")
-    reason = args.splice(1).join(" ") | `Muted by ${msg.author.tag}`
+    if(!msg.guild.member(client.user.id).permissions.has("MANAGE_ROLES")) return msg.channel.send("I don't have permission to do this.");
+    if(!msg.guild.member(msg.author.id).permissions.has("MANAGE_ROLES")) return msg.channel.send("You don't have permission to do this.");
+    let id_to_mute = args[0].replace(/[<@!>]/g, "")
+    let reason = args.splice(1).join(" ")
+    if (reason == "") {
+        reason = `Muted by ${msg.author.tag}`
+    }
     msg.guild.fetchMember(id_to_mute).then(member => {
         if(!msg.guild.roles.map(r => r.name.toLowerCase()).includes("muted")) return msg.channel.send("You do not have a role named `Muted`");
-        if(member.roles.find(role => role.name.toLowerCase() == "muted")) return msg.channel.send("User is not muted");
+        if(member.roles.find(role => role.name.toLowerCase() == "muted")) return msg.channel.send("User is already muted");
         role = msg.guild.roles.find(role => role.name.toLowerCase() == "muted");
         msg.member.addRole(role, reason).then(user => {
-            return msg.channel.send(`User ${user.tag} has been muted for: ${reason}`)
+            return msg.channel.send(`User ${user.user.tag} has been muted for: ${reason}`)
         }).catch(e => {
             if(e.message == "Missing Permissions") {
                 return msg.channel.send(`I can not mute that person as I lack the permission to do so.\nIs the user higher or the same permission as me?`)
