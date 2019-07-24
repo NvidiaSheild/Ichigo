2718ceb6835bf73d64fcf23e400a9f3041ed468a
Luke-6723 <lukemorris2015@hotmail.com>
2019-07-24 21:11:52 +0100

    added exec command

---
+++ commands/exec.js
@@ -0,0 +1,24 @@
+let settings = require('../settings')
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const clean = text => {
+    if (typeof (text) === 'string') {return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));}
+    else {return text;}
+};
+
+exports.run = (client, msg, args, server_settings) => {
+    if(!settings.eval_users.includes(msg.author.id)) return;
+    const code = args.join(' ');
+    exec(code).then(output => {
+        msg.channel.send('```js\n' + clean(output.stdout) + '\n```');
+    }).catch((stderr) => {
+        msg.channel.send(`\`ERROR\` \`\`\`xl\n${stderr}\n\`\`\``);
+    });
+}
+
+exports.info = {
+  "name": "exec",
+  "type": "owner",
+  "description": "execute scripts on the bots host."
+}
