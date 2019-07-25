5da34bf503194b79bb174e8d0e76415fced44a2b
Luke-6723 <lukemorris2015@hotmail.com>
2019-07-25 07:13:41 +0100

    added profile command

---
+++ commands/profile.js
@@ -0,0 +1,15 @@
+let DiscordEmbed = require('discord.js').RichEmbed
+
+exports.run = (client, msg, args, server_settings) => {
+    let embed = new DiscordEmbed()
+    .setImage("https://fanaticphoenix.co.uk/user_profile/" + msg.author.id + "?cache=" + (Math.random()*2384179847))
+    .setFooter("Try command again if profile doesnt show properly", client.avatarURL)
+    return msg.channel.send({embed})
+}
+
+exports.info = {
+    "name": "profile",
+    "description": "get your user profile",
+    "example": "profile",
+    "type": "basic"
+}
