df31f35efcf3dc67d4ebc976f2d4049e7b04b210
Luke-6723 <lukemorris2015@hotmail.com>
2019-08-08 23:39:44 +0100

    say command user array

--- settings.example.js
+++ settings.example.js
@@ -1,6 +1,7 @@
 exports.token = "";
 exports.default_prefix = "";
 exports.eval_users = [ "IDs are strings" ];
+exports.say_users = [ "IDs are strings" ];
 exports.weebsh = "";
 exports.vote_lock = false;
 exports.guild_update_webhook = "";
