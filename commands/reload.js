bcb5418900475470e58e3c35e4dcec89ff118cde
Luke-6723 <lukemorris2015@hotmail.com>
2019-09-19 21:16:21 +0100

    made reload show actual shards

--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,6 @@
 const settings = require('../settings')
 
-exports.reload = (command_to_reload) => { 
+exports.reload = (command_to_reload) => {
     try {
         require.resolve(`${process.cwd()}/commands/${command_to_reload}.js`)
     } catch (e) {
@@ -9,29 +9,28 @@ exports.reload = (command_to_reload) => {
     return delete require.cache[require.resolve(`${process.cwd()}/commands/${command_to_reload}.js`)];
 }
 
-exports.run = (client, msg, args, server_settings) => { 
-        if (!settings.eval_users.includes(msg.author.id)) return;
-        try {
-            let command_to_reload = args[0]
-            let require_dir = process.cwd().replace("\\", "/").replace("\\", "/");
-            client.shard.fetchClientValues('shard.id').then(shard_ids => {
-                client.shard.broadcastEval(`require("${require_dir}/commands/reload").reload("${command_to_reload}")`).then(results => {
-                    let text = `[Shard]\t[Reloaded]\n`;
-                    let out_txt = shard_ids.forEach(shard => {
-                        text += `[Shard ${shard}]:\t ${results[shard]}\n`;
-                        });
-                    msg.channel.send(text, {split: true, code:"prolog"});
-                })
-            })
-        } catch(e) {
-            client.logs.error(e)
-            if (e.code == "MODULE_NOT_FOUND") {
-                return msg.channel.send("That command doesn't exist")
-                
-            } else {
-                reject(e)
-            }
+exports.run = (client, msg, args, server_settings) => {
+    if (!settings.eval_users.includes(msg.author.id)) return;
+    try {
+        let command_to_reload = args[0]
+        let require_dir = process.cwd().replace("\\", "/").replace("\\", "/");
+        shard_ids = client.ws.shards.map(sh => sh.id)
+        client.shard.broadcastEval(`require("${require_dir}/commands/reload").reload("${command_to_reload}")`).then(results => {
+            let text = `[Shard]\t[Reloaded]\n`;
+            let out_txt = shard_ids.forEach(shard => {
+                text += `[Shard ${shard}]:\t ${results[shard]}\n`;
+            });
+            msg.channel.send(text, { split: true, code: "prolog" });
+        })
+    } catch (e) {
+        client.logs.error(e)
+        if (e.code == "MODULE_NOT_FOUND") {
+            return msg.channel.send("That command doesn't exist")
+
+        } else {
+            reject(e)
         }
+    }
 }
 
 exports.info = {
